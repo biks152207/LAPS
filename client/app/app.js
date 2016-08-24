6269f3cae1340d76e30af496bdf6270fa2e9a04f
biks152207 <bikrambasnet1@gmail.com>
2016-08-24 14:50:21 +0545

    change

--- client/app/app.js
+++ client/app/app.js
@@ -7,4 +7,18 @@ angular.module('lapsApp', ['lapsApp.auth', 'lapsApp.admin', 'lapsApp.constants',
     $urlRouterProvider.otherwise('/');
 
     $locationProvider.html5Mode(true);
-  });
+  })
+  .run(['$rootScope', function($rootScope){
+    // Executes when the transition to certain routes start
+    $rootScope.$on('$stateChangeStart', function(e, toState, toParams, fromState, fromParams){
+      setLoadingState('start');
+    });
+     // Executes when the transition to the page is successful
+    $rootScope.$on('$stateChangeSuccess', function(e, toState, toParams, fromState, fromParams){
+      setLoadingState('end');
+    });
+
+    var setLoadingState = function(state){
+      state == 'start' ? $rootScope.isLoading = true: $rootScope.isLoading = false;
+    }
+  }])
